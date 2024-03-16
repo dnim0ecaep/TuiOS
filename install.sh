42e4fd4aa694c9ce1d6944cc99b7c04258f964f8
Mrmateromatt <dnim0ecaep@gmail.com>
2024-03-16 09:54:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ wget https://github.com/directvt/vtm/releases/latest/download/vtm_linux_x86_64.z
 unzip vtm_linux_x86_64.zip
 tar -xvf  vtm_linux_x86_64.tar
 ./vtm -i
-apt install lynx cmus mc aewan calcurse mutt sc-im wavemon termshark netris nsnake -y
+apt install gpm lynx cmus mc aewan calcurse mutt sc-im wavemon termshark netris nsnake -y
 mkdir ~/.config/vtm
 cp system.xml ~/.config/vtm/
