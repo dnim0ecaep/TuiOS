d867450a6775c69d91f0948391d8fee6cfbc1955
Mrmateromatt <dnim0ecaep@gmail.com>
2024-03-16 09:01:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-sudo apt install  wget
+apt install  wget
 wget https://github.com/directvt/vtm/releases/latest/download/vtm_linux_x86_64.zip
 unzip vtm_linux_x86_64.zip
 tar -xvf  vtm_linux_x86_64.tar
 ./vtm -i
 apt install lynx cmus mc aewan calcurse mutt sc-im wavemon termshark netris nsnake -y
+mkdir ~/.config/vtm
+cp system.xml ~/.config/vtm/
