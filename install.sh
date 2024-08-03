1169f82ff5f6e40d5b1777a208826ae329e932ac
Mrmateromatt <dnim0ecaep@gmail.com>
2024-08-03 08:15:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #this needs to be run as sudo 
 apt install  wget
+# wget https://github.com/directvt/vtm/releases/download/v0.9.98/vtm_linux_arm64.zip
 wget https://github.com/directvt/vtm/releases/latest/download/vtm_linux_x86_64.zip
 unzip vtm_linux_x86_64.zip
 tar -xvf  vtm_linux_x86_64.tar
