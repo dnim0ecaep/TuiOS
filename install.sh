663887f2230da3e3709cbf646aaea23e40e131f6
Mrmateromatt <dnim0ecaep@gmail.com>
2025-10-05 08:58:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ wget https://github.com/directvt/vtm/releases/latest/download/vtm_linux_x86_64.z
 unzip vtm_linux_x86_64.zip
 tar -xvf  vtm_linux_x86_64.tar
 ./vtm -i
-sudo apt install -y gpm lynx cmus mc aewan calcurse mutt sc-im wavemon termshark netris nsnake -y
-mkdir /etc/vtm
-cp settings.xml /etc/vtm/
-sudo echo 'SUBSYSTEM=="input", KERNEL=="mice", MODE="666"' > /etc/udev/rules.d/85-vtm-mouse-access.rules
-sudo udevadm control --reload-rules && udevadm trigger
+sudo apt install -y eza gpm lynx cmus mc aewan calcurse mutt sc-im wavemon termshark nsnake
+sudo mkdir /etc/vtm
+mkdir ~/.config/vtm/
+#sudo cp settings.xml /etc/vtm/
+cp settings.xml ~/.config/vtm/
+#sudo echo 'SUBSYSTEM=="input", KERNEL=="mice", MODE="666"' > /etc/udev/rules.d/85-vtm-mouse-access.rules
+#sudo udevadm control --reload-rules && udevadm trigger
